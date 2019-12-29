4af93a060d6c19b642d0aebc310b554cd9fab403
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-12-29 21:03:26 +0900

    modify App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from "react";
-import WthrApp from "./Components/Whtr/WthrApp";
+
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <WthrApp />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
