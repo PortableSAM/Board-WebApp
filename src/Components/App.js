9e57d6862d4089ae83a4e0fadb8d9b448f4f15b0
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2020-01-03 15:32:03 +0900

    index / App JS file 수정

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "../Styles/App.css";
+import WeatherApp from "./WeatherApp/WeatherApp";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App-container">
+      <WeatherApp />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/WeatherApp/WeatherApp.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import "../../Styles/Weather.css";
+
+function WeatherApp() {
+  return (
+    <section className="weather-container">
+      <header>
+        <h2>Weather App</h2>
+      </header>
+    </section>
+  );
+}
+
+export default WeatherApp;
