f1b611fd024d23c6337ad9d319acb7497e929378
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2020-01-03 17:56:12 +0900

    WeatherApp 작성 중 및 Weather Info 생성

--- src/Components/WeatherApp/WeatherApp.js
+++ src/Components/WeatherApp/WeatherApp.js
@@ -1,12 +1,87 @@
 import React, { useState } from "react";
+import WeatherInfo from "./WeatherInfo";
 import "../../Styles/Weather.css";
 
+const App_ID = "a356aa8551d9437c931d80de6c06c0fa";
+
+const callApiUrl = query =>
+  `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${App_ID}&units=metric`;
+
+const cityRegx = /[a-z]/;
+
+const validateCity = city => cityRegx.test(city);
+
 function WeatherApp() {
+  const [serchQuery, setSerchQuery] = useState("");
+  const [weatherData, setWeatherData] = useState({
+    temp: undefined,
+    maxTemp: undefined,
+    minTemp: undefined,
+    country: undefined,
+    city: undefined,
+    desc: undefined,
+    speed: undefined,
+    deg: undefined
+  });
+  const [isValidCity, setIsValidCity] = useState(true);
+  function updateSerchQuery(event) {
+    setSerchQuery(event.target.value);
+    setIsValidCity(
+      event.target.value === "" || validateCity(event.target.value)
+    );
+  }
+  async function getWeather() {
+    if (!isValidCity || serchQuery === "") {
+      setIsValidCity(false);
+      return;
+    }
+
+    const response = await fetch(callApiUrl(serchQuery));
+    if (!response.ok) {
+      return;
+    }
+
+    const { main, sys, name, weather, wind } = await response.json();
+    setWeatherData({
+      temp: main.temp,
+      maxTemp: main.temp_max,
+      minnTemp: main.temp_min,
+      country: sys.country,
+      city: name,
+      desc: weather[0].main,
+      speed: wind.speed,
+      deg: wind.deg
+    });
+  }
+
   return (
     <section className="weather-container">
-      <header>
-        <h2>Weather App</h2>
+      <header className="weather-header">
+        <h2>Weather Info</h2>
+        <div>
+          <input
+            placeholder="City"
+            className="serch-input"
+            onChange={updateSerchQuery}
+            type="text"
+          />
+          <button
+            onClick={getWeather}
+            type="button"
+            className="btn btn-outline-primary"
+          >
+            Get Weather
+          </button>
+        </div>
       </header>
+      <p className="error">{isValidCity ? "" : "Invalid City Name"}</p>
+      <section className="weather-info">
+        {weatherData.temp ? (
+          <WeatherInfo data={weatherData} />
+        ) : (
+          <p> No Weather to Display</p>
+        )}
+      </section>
     </section>
   );
 }
---
+++ src/Components/WeatherApp/WeatherInfo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function WeatherInfo() {
+  return <div>info</div>;
+}
+
+export default WeatherInfo;
